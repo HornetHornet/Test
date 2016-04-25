52fd852f43e5c66f392199b674760f155e1fd0fa
HornetHornet <epoxyde3000@gmail.com>
2016-04-25 14:08:07 +0300

    Release

--- Training/generate_scripts_and_folders.bat
+++
@@ -1,8 +0,0 @@
-"C:\Test\Training\CreateScriptsAndFolders\x64\Debug\CreateScriptsAndFolders.exe"^
- "C:\opencv\build\x64\vc14\bin\opencv_createsamples.exe"^
- "C:\opencv\build\x64\vc14\bin\opencv_traincascade.exe"^
- "C:\Test\DataSet_pic\reference"^
- "C:\Test\Training"^
- "C:\Test\Training\vectors"^
- "C:\Test\Training\cascades"
- pause
--- Training/show_vec.bat
+++
@@ -1,5 +0,0 @@
-"C:\opencv\build\x64\vc14\bin\opencv_createsamples.exe"^
- -vec vectors/1.vec^
- -w 60^
- -h 60
- pause
